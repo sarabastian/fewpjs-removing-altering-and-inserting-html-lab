181ed7809b76c26eeceb3430cf9b8673c0aca8fe
sarabastian <seb118@georgetown.edu>
2020-11-17 10:52:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+var element = document.getElementById('main');
+element.remove();
+
+
+let newHeader = document.createElement("h1");
+newHeader.className = 'victory'
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Sara is the champion' 
+
+
